7979a56333917a66af67a2d86bebbc390ece771b
André Bez Birolo <andrecentrobrasileiro@outlook.com>
2021-09-22 14:28:57 -0300

    Validação padrão

--- src/app/home/new-user/new-user.component.ts
+++ src/app/home/new-user/new-user.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NewUserService } from './new-user.service';
+import { NewUser } from './new-user';
 
 @Component({
   selector: 'app-new-user',
@@ -7,14 +10,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewUserComponent implements OnInit {
 
+  newUserForm!: FormGroup;
+
   userName: string = '';
   fullName: string = '';
   password: string = '';
   email: string = '';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private formBuilder: FormBuilder,
+    private newUserService: NewUserService
+  ) {
   }
 
+  ngOnInit(): void {
+    this.newUserForm = this.formBuilder.group({
+      email: ['',
+        [
+          Validators.required,
+          Validators.email
+        ]
+      ],
+      fullName: ['',
+        [
+          Validators.required,
+          Validators.minLength(4)
+        ]
+      ],
+      userName: ['',
+        [
+          Validators.required
+        ]
+      ],
+      password: ['',
+        [
+          Validators.required
+        ]
+      ],
+    });
+  }
+
+  register() {
+    const newUser = this.newUserForm.getRawValue() as NewUser;
+    console.log(newUser)
+  }
 }
