58ce8ab98b117d7814ff3c940c56babeb8793b83
André Bez Birolo <andrecentrobrasileiro@outlook.com>
2021-09-22 13:23:20 -0300

    Formulário template-driven

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  userName: string = '';
+  password: string = '';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  login() {
+    console.log(this.userName, this.password)
   }
 
 }
