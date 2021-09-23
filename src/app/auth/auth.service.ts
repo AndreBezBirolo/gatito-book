f90b639af236080a600b70c306ece0bf28a6ee11
André Bez Birolo <andrecentrobrasileiro@outlook.com>
2021-09-23 09:32:04 -0300

    Refatorado o serviço de autenticação

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,19 +1,36 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { UserService } from './user/user.service';
+import { tap } from 'rxjs/operators';
 
 const API_URL = environment.API_URL
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private userService: UserService
+  ) {
+  }
 
-  authUser(userName: string, password: string): Observable<any> {
-    return this.http.post(API_URL + '/user/login', {
-      userName, password
-    })
+  authUser(userName: string, password: string): Observable<HttpResponse<any>> {
+    return this.http
+      .post(API_URL + '/user/login', {
+          userName, password
+        }, {
+          observe: 'response'
+        }
+      )
+      .pipe(
+        tap((res) => {
+          const authToken = res.headers.get('x-access-token') ?? '';
+          this.userService.saveToken(authToken)
+        })
+      )
   }
 }
